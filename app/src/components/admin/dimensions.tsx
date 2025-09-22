fc80e2068af9ce1c1f0140b22e25551531cd11d7
Kristian Väisänen <kippeves@gmail.com>
2025-09-22 14:49:00 +0200

    Add Dimensions-input

---
+++ app/src/components/admin/dimensions.tsx
@@ -0,0 +1,46 @@
+"use client";
+import { Dimensions } from "@/lib/types";
+import React, { Fragment, useEffect, useState } from "react";
+
+export default function DimensionInput({
+  initialValues = {
+    depth: 0,
+    height: 0,
+    width: 0
+  },
+  onUpdates
+}: {
+  initialValues?: Dimensions;
+  onUpdates: (updatedValues: Dimensions) => void;
+}) {
+  const [dimensions, setDimensions] = useState<Dimensions>(initialValues);
+  function setValue({ key, value }: { value: string; key: string }): void {
+    setDimensions(prev => ({ ...prev, [key]: +value }));
+  }
+
+  useEffect(() => onUpdates(dimensions), [dimensions]);
+
+  return (
+    <div className="flex flex-col gap-2 w-fit items-center p-2">
+      {Object.entries(dimensions).map(([key, value]) => (
+        <div key={key} className="flex flex-col w-40">
+          <label className="capitalize m-0" htmlFor={`dimensions_${key}`}>
+            {key}
+          </label>
+          <div className="flex gap-2 items-center">
+            <input
+              className="border p-1 rounded grow w-20"
+              id={`dimensions_${key}`}
+              name={`dimensions_${key}`}
+              min={0}
+              onChange={e => setValue({ key, value: e.target.value })}
+              defaultValue={value}
+              type="number"
+            />
+            <span>Cm.</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
+}
