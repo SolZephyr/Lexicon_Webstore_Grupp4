5c3a550c0973a9e5679fce4737a5462e28db3004
Kristian Väisänen <kippeves@gmail.com>
2025-09-24 13:44:57 +0200

    Create Update-page.

--- app/src/app/admin/[id]/page.tsx
+++ app/src/app/admin/[id]/page.tsx
@@ -1,4 +1,24 @@
+import ProductForm from "@/components/admin/product-form";
+import { ContentWrapper } from "@/components/content-wrapper";
+import { getProduct } from "@/lib/data/products";
+import { Edit } from "../actions";
 
-export default function ProductPage() {
-    return <div>Product Page</div>;
+type Props = { params: Promise<{ [key: string]: string | undefined }> };
+
+export default async function ProductPage(props: Props) {
+  const params = await props.params;
+  const productId = params.id ? Number(params.id) : undefined;
+
+  if (!productId) return { title: "Not Found" };
+
+  const product = await getProduct(productId);
+  return (
+    <ContentWrapper>
+      <ProductForm
+        initialState={product}
+        serverAction={Edit}
+        submitButtonText='Update Product'
+      />
+    </ContentWrapper>
+  );
 }
