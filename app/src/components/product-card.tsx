3cd36bd4fc43e2dbf8e8b53943824437f69ae9b3
Alexander <32795918+Alexeri@users.noreply.github.com>
2025-09-26 12:55:51 +0200

    fix image overlapping badge

--- app/src/components/product-card.tsx
+++ app/src/components/product-card.tsx
@@ -5,30 +5,33 @@ import StockStatus from "./stock-status";
 import Link from "next/link";
 import { Star, StarHalf } from "lucide-react";
 import { Badge } from "@/components/ui/badge";
-import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function ProductCard({ product }: { product: ThinProduct }) {
   return (
     <article className="contents">
       <Link
         href={`/products/${product.id}`}
-        className="grid grid-rows-subgrid gap-1 row-span-4 focus-within:border-gray-300 hover:border-gray-100 transition-colors rounded group"
+        className="grid grid-rows-subgrid gap-1 row-span-4 focus-within:border-gray-300 transition-colors rounded group"
       >
         <div className="relative flex justify-center items-center bg-accent rounded">
           {product.discountPercentage > 0 && (
-            <div className="absolute top-2 left-2">
-              <Badge
-                variant="destructive"
-                className="rounded px-2 py-1 text-xs font-semibold"
-              >
-                -{Math.round(product.discountPercentage)}%
-              </Badge>
-            </div>
+            <Badge
+              variant="destructive"
+              className="rounded px-2 py-1 text-xs font-semibold absolute top-2 left-2 z-50"
+            >
+              -{Math.round(product.discountPercentage)}%
+            </Badge>
           )}
           <Image
             src={product.thumbnail ?? "/placeholder-image.svg"}
             alt={product.title}
-            className="w-full object-cover rounded p-4 group-hover:scale-[1.02] transition-all"
+            className="w-full object-cover rounded p-4 group-hover:scale-[1.02] transition-transform"
             priority
             width={300}
             height={300}
@@ -36,7 +39,7 @@ export default function ProductCard({ product }: { product: ThinProduct }) {
         </div>
         <div className="flex justify-between items-center px-2 py-1">
           <Rating rating={product.rating} />
-          <StockStatus amount={product.stock} showTooltip/>
+          <StockStatus amount={product.stock} showTooltip />
         </div>
         <h2 className="text-md font-semibold px-2 py-1">{product.title}</h2>
         <div className="px-2 pt-1 pb-3">
@@ -68,7 +71,12 @@ function Rating({ rating }: { rating: number }) {
           <div className="flex items-center gap-0.5 text-yellow-500  py-2">
             {/* full stars */}
             {[...Array(fullStars)].map((_, i) => (
-              <Star key={`full-${i}`} size={16} fill="currentColor" strokeWidth={0} />
+              <Star
+                key={`full-${i}`}
+                size={16}
+                fill="currentColor"
+                strokeWidth={0}
+              />
             ))}
 
             {/* half star with background */}
@@ -93,7 +101,12 @@ function Rating({ rating }: { rating: number }) {
 
             {/* empty stars */}
             {[...Array(emptyStars)].map((_, i) => (
-              <Star key={`empty-${i}`} size={16} fill="none" strokeWidth={1.5} />
+              <Star
+                key={`empty-${i}`}
+                size={16}
+                fill="none"
+                strokeWidth={1.5}
+              />
             ))}
           </div>
         </TooltipTrigger>
