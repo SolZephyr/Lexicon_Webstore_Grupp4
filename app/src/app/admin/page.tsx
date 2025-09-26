558c7137769db4a7b8de023d2e522c8f80464411
Kristian Väisänen <kippeves@gmail.com>
2025-09-26 11:09:08 +0200

    Add button for creating product.

--- app/src/app/admin/page.tsx
+++ app/src/app/admin/page.tsx
@@ -2,6 +2,8 @@ import { getFullProductsByFilter } from "@/lib/data/products";
 import { Suspense } from "react";
 import Loader from "@/components/loader";
 import TableLoader from "@/components/admin/table-loader";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default async function AdminPage() {
   const task = getFullProductsByFilter({ limit: 100 });
@@ -9,6 +11,11 @@ export default async function AdminPage() {
   return (
     <div className="flex flex-col gap-2">
       <h1 className="text-xl font-bold">AdminPage</h1>
+      <div className="flex flex-start">
+        <Button variant={"brand"} asChild>
+          <Link href="/admin/create">Create Product</Link>
+        </Button>
+      </div>
       <Suspense fallback={<Loader />}>
         <TableLoader task={task} />
       </Suspense>
